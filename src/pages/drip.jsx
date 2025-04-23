574898c683be024410dbcdea1ee083f79195be7d
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-23 16:47:08 -0300

    feat: global and modularized CSS

---
+++ src/pages/drip.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Drip.module.css'
+
+export default function Drip() {
+
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    )
+
+}
---
+++ src/pages/jsx.jsx
@@ -0,0 +1,24 @@
+export default function Jsx() {
+
+    const a = 7;
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const results = []; 
+
+    numbers.forEach((num) => {
+        results.push(num * a);
+    });
+
+    return (
+        <div>
+            <h1>JSX é um conceito Central</h1>
+            <h2>Multiplicando por 7:</h2>
+            <ul>
+                {results.map((result, index) => (
+                    <li key={index}>
+                        {numbers[index]} x 7 = {result}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
